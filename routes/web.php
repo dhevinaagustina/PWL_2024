969cd66d0774f01b5ddd24cbae6c7380d26e84a6
dhevinaagustina <dhevinaagustina@gmail.com>
2025-02-18 21:59:10 +0700

    Update Praktikum Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return ('Hello World');
 });
+
+// Route::get('/world', function () {    
+//     return 'World'; 
+// });
+
+// Route::get('/welcome', function () {    
+//     return 'Selamat Datang'; 
+// });
+
+// Route::get('/about', function () {    
+//     return 'NIM: 2341760065,
+//             Nama: Dhevina Agustina'; 
+// });
+
+// // Route::get('/user/{name}', function ($name) {    
+// //     return 'Dhevina Agustina ' . $name;
+// // });
+
+// Route::get('/posts/{post}/comments/{comment}', function 
+// ($postId, $commentId) { 
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; }); 
+
+// Route::get('/articles/{id}', function ($id) {
+//      return 'Halaman Artikel dengan ID ' . $id;
+// });
+    
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+// Route::get('/user/{name?}', function ($name = 'John') {
+//     return 'Nama Saya ' . $name;
+// });
+
+
+
+
