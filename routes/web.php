415a5ccd71905401ca3d3587bc3a0cf79e5423c6
dhevinaagustina <dhevinaagustina@gmail.com>
2025-02-21 19:59:43 +0700

    commit perubahan praktikum controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+// use App\Http\Controllers\WelcomeController;
+// use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController; 
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +20,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', function () {
-    return ('Hello World');
-});
+Route::get('/', HomeController::class);
+
+Route::get('/about', AboutController::class);
+
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class); 
+
+Route::resource('photos', PhotoController::class)->only([ 
+    'index', 'show' 
+]); 
+
+Route::resource('photos', PhotoController::class)->except([ 
+    'create', 'store', 'update', 'destroy' ]); 
+
+
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Route::get('/hello', [WelcomeController::class,'hello']); 
+// Route::get('/hello', function () {
+//     return ('Hello World');
+// });
 
 // Route::get('/world', function () {    
 //     return 'World'; 
 // });
 
-// Route::get('/welcome', function () {    
+// Route::get('/welcome', function () {         
 //     return 'Selamat Datang'; 
 // });
 
